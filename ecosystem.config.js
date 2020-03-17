c113f4fa4ddac49bce34cecfc271b9615d47a316
suzhihui <wisdom.su@163.com>
2020-03-17 12:04:58 +0800

    add port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,8 @@ module.exports = {
     watch: true,
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: 'development',
+      PORT: 8889
     },
     env_production: {
       NODE_ENV: 'production'
