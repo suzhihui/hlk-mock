54d50878bfae145d362dc3d925e08b60683e70d7
suzhihui <wisdom.su@163.com>
2020-03-17 11:42:23 +0800

    add build command

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+module.exports = {
+  apps : [{
+    name: 'hlkapi',
+    script: path.resolve(__dirname, './dist/index.js'),
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: require('os').cpus().length,
+    autorestart: true,
+    watch: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
